a9ef65a5479e5f42b060fa91c064320bfcaeb36b
Eric Le <eric4leformal@gmail.com>
2025-09-21 21:17:15 -0700

    set up app, file and folder structure, routiing, and auth pages

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,7 @@
+import AuthForm from "@/components/AuthForm";
+
+export default function SignUP() {
+  return (
+    <AuthForm type="sign-up" />
+  );
+}
---
+++ components/FormField.tsx
@@ -0,0 +1,41 @@
+import { Button } from "@/components/ui/button";
+import {
+  FormControl,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Control, Controller, FieldValues } from "react-hook-form";
+
+interface FormFieldProps<T extends FieldValues> {
+  control: Control<T>;
+  name: Path<T>;
+  label: string;
+  placeholder?: string;
+  type?: "text" | "email" | "password" | "file";
+}
+
+export default function FormField({
+  control,
+  name,
+  label,
+  placeholder,
+  type = "text",
+}: FormFieldProps<T>) {
+  return (
+    <Controller
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel className="label">{label}</FormLabel>
+          <FormControl>
+            <Input className="input" placeholder={placeholder} type={type} {...field} />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+}
